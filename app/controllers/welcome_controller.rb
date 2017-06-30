07f32fed8e5f8f3de567e9a1703b26fed3cc0dc1
Siddharth Krish <siddharth.krish@moneysmart.sg>
2017-06-30 09:07:51 +0800

    added yields

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def hello
+    @alert_message = "hello there"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/main'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
